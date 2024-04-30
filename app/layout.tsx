51bbc33e60ab34adade656e6a91627f096121c7b
Mahmoud Abdelfatah <mahmoud.abdelfatah@fedis.com.eg>
2024-05-01 00:12:46 +0300

    update font v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./style/style.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
         <ToastContainer />
       </body>
