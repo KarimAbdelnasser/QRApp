9fd8cb72429458414834e2ee52fbdd4ee253cc23
EyadHamed <eyad.hamed@fedis.com.eg>
2024-05-19 18:08:54 +0300

    change card no and styliing

--- app/components/QRCard.tsx
+++ app/components/QRCard.tsx
@@ -5,22 +5,11 @@ import Link from "next/link";
 
 const QRCard: React.FC = () => {
   const qrUrls = [
-    `https://cardnames.vercel.app/valid?name=username&cardNumber=2121028790002407&isValid=true`,
-    `https://cardnames.vercel.app/valid?name=username&cardNumber=9445944611516120&isValid=false`,
-    `https://cardnames.vercel.app/valid?name=username&cardNumber=6859093107986957&isValid=true`,
-    `https://cardnames.vercel.app/valid?name=username&cardNumber=1431580449321734&isValid=false`,
-    `https://cardnames.vercel.app/valid?name=username&cardNumber=9758377599758158&isValid=true`,
-    `https://cardnames.vercel.app/valid?name=username&cardNumber=4411620538763908&isValid=false`,
+    `/valid?name=username&cardNumber=9445944611516120&isValid=true`,
+    `/valid?name=username&cardNumber=2121028790002407&isValid=false`,
   ];
 
-  const cardLabels = [
-    { name: "Eyad Hamed", number: "2121 02879 0002 407" },
-    { name: "Mohamed Ali", number: "9445 94461 1516 120" },
-    { name: "Youssef Osama", number: "6859 0931 07986 957" },
-    { name: "Ziad Ahmed", number: "1431 5804 4932 1734" },
-    { name: "Saif Ashraf", number: "9758 3775 9975 8158" },
-    { name: "Omar Mostafa", number: "4411 6205 3876 3908" },
-  ];
+  const cardLabels = [{ number: "0000 0000 " }, { number: "2121 0287" }];
 
   return (
     <div className="card-container">
@@ -30,9 +19,7 @@ const QRCard: React.FC = () => {
             <QRCode value={url} className="qr-img" />
           </div>
           <div className="card-content">
-            <h2>Offer Card</h2>
-            <h4>Name: {cardLabels[index].name}</h4>
-            <h5>Card Number: {cardLabels[index].number}</h5>
+            <h2 style={{marginTop:"50px"}}>Card Number: {cardLabels[index].number}</h2>
           </div>
         </Link>
       ))}
